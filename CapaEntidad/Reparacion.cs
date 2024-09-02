d2f5ad34f2f426a7c00b7ed96fd447b12c8391a3
angeld1603 <angeldavidjulioorozco58@gmail.com>
2024-09-02 18:13:07 -0500

    Update LoginController.cs

---
+++ CapaEntidad/Reparacion.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Reparacion
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public DateTime FechaEntregaPrevista { get; set; }
+
+        public string Estado { get; set; }
+
+        public string VehiculoId { get; set; } // Para identificar el vehículo
+
+        public string MecanicoId { get; set; } // Para identificar al mecánico asignado
+
+        public List<string> Repuestos { get; set; } // Lista de repuestos asignados
+    }
+}
